7784a80c5807d233bdb881c542277a17202c64bd
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-27 16:32:39 +0000

    use recursion to calculate nested for loops

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// not solved
 function sumItems(array) {
-  // Sum all the numbers in the array
+
+  let sum = 0
+ array.forEach((item) => {
+   if(Array.isArray(item)) {
+     sum += sumItems(item)
+   }
+   else {
+    sum += item
+   }
+ })
+
+  return sum
 }
 
+console.log(sumItems([1, [2, 3], 4, [], 5]));
 module.exports = sumItems;
